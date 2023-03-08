23a87317ac089f3fee8f1b94686a065f84e23f03
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-03-08 13:01:50 +0600

    Navbar added

---
+++ src/Router.js
@@ -0,0 +1,25 @@
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("./layout/Main");
+const { default: About } = require("./Pages/About");
+const { default: Card } = require("./Pages/Card");
+const { default: Home } = require("./Pages/Home");
+const { default: TopRated } = require("./Pages/TopRated");
+
+export const router = createBrowserRouter([
+    {
+        path: '/', element: <Main></Main>, children: [
+            {
+                path: '/', element: <Home></Home>
+            },
+            {
+                path: '/about', element: <About></About>
+            },
+            {
+                path: '/top-rated', element: <TopRated></TopRated>
+            },
+            {
+                path: '/card', element: <Card></Card>
+            },
+        ]
+    }
+])
